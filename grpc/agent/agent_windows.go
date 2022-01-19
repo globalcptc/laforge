bbb39bf60531b559292b992a365fb7b6d40e30b1
Curtis Fowler <caf203@gmail.com>
2022-01-18 21:49:39 -0500

    feat: some windows validations + able to "go run"

--- grpc/agent/agent_windows.go
+++ grpc/agent/agent_windows.go
@@ -1,16 +1,23 @@
-//go:build windows
+windows
 // +build windows
 
-// package main
+package p
+// package main // uncomment for testing funcs below
 
 import (
+	"crypto/md5"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
+	"net"
 	"net/http"
 	"os"
 	s "os"
 	"os/exec"
+	user "os/user"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"syscall"
 	"time"
@@ -28,7 +35,7 @@ func RebootSystem() {
 
 	r1, _, _ := exitwin.Call(0x02, 0)
 	if r1 != 1 {
-		ExecuteCommand("cmd", "/C", "shutdown", "/r", "/f")
+		SystemExecuteCommand("cmd", "/C", "shutdown", "/r", "/f")
 	}
 
 	time.Sleep(1 * time.Hour) // sleep forever bc we need to restart
@@ -133,16 +140,24 @@ func GetSystemDependencies() []string {
 	return []string{}
 }
 
-// Validate file system
-
 // Validation functions
 func GetNetBanner(portnum int64) (bool, error) { // exists (boolean)
 	return true, nil
 }
 
-func NetHttpContentRegex(full_url string) (string, error) {
+func NetHttpContentRegex(full_url string) (string, error) { // content hash (string)
+	net_resp, err := http.Get(full_url)
+	if err != nil {
+		return "", err
+	}
+	defer net_resp.Body.Close()
+	page_html, deserialize_err := ioutil.ReadAll(net_resp.Body)
+	if deserialize_err != nil {
+		return "", deserialize_err
+	}
 
-	return "false", nil
+	// return MD5Sum(fmt.Sprintf("%s", page_html)), nil
+	return string(page_html[:]), nil // stringify
 }
 
 func FileExists(file_location string) (bool, error) {
@@ -171,10 +186,11 @@ func FileContentRegex(file_location string) (string, error) { // page content to
 		return "", read_err
 	}
 	file_hash := md5.New()
-	if body_contents, err := io.Copy(file_hash, file_read); err != nil {
+	_, err := io.Copy(file_hash, file_read)
+	if err != nil {
 		log.Fatal(err)
 	}
-	return body_contents, nil
+	return fmt.Sprintf("%x", file_hash.Sum(nil)), nil
 }
 
 func DirectoryExists(directory string) (bool, error) { // exists (boolean)
@@ -190,8 +206,9 @@ func UserExists(user_name string) (bool, error) { // exists (boolean
 	if err != nil {
 		return false, err
 	}
+	fmt.Println(users)
 	for _, user := range users {
-		if u.Username == user_name {
+		if user.Username == user_name {
 			return true, nil
 		}
 	}
@@ -199,48 +216,15 @@ func UserExists(user_name string) (bool, error) { // exists (boolean
 }
 
 func UserGroupMember(user_name string, group_name string) (bool, error) { // is in the group or not (boolean)
-	// TODO: need a special way to get this via windows
-	// group_contents, read_err := ioutil.ReadFile("/etc/group")
-	if read_err != nil {
-		return false, read_err
-	}
-	groups := strings.Split(string(group_contents), "\n")
-	for i := 0; i < len(groups); i++ {
-		// example groups
-		/*
-			adm:x:4:piero
-			tty:x:5:
-			disk:x:6:
-			lp:x:7:
-			mail:x:8:
-			news:x:9:
-			uucp:x:10:
-			man:x:12:
-			proxy:x:13:
-			kmem:x:15:
-			dialout:x:20:
-			fax:x:21:
-			voice:x:22:
-			cdrom:x:24:piero
-		*/
-		group_line_chunks := strings.Split(string(group_contents), ":")
-		if group_line_chunks[0] == group_name && len(group_line_chunks) > 3 {
-			// first part of /etc/group entry matches and there are users assigned to the group
-			users_in_group := strings.Split(group_line_chunks[3], ",")
-			for j := 0; j < len(users_in_group); j++ {
-				if users_in_group[j] == user_name {
-					return true, nil
-				}
-			}
-		}
-		return false, nil
-	}
+	fmt.Println(user.Lookup(user_name))
 	return false, nil
 }
 
 // https://stackoverflow.com/questions/56336168/golang-check-tcp-port-open
+// https://stackoverflow.com/questions/56336168/golang-check-tcp-port-open
 func HostPortOpen(port int64) (bool, error) { // exists (boolean)
-	conn, err := net.DialTimeout("tcp", net.JoinHostPort("127.0.0.1", string(port)), time.Second)
+
+	conn, err := net.DialTimeout("tcp", net.JoinHostPort("127.0.0.1", strconv.Itoa(int(port))), 10*time.Second)
 	if err != nil {
 		return false, err
 	}
@@ -253,8 +237,7 @@ func HostPortOpen(port int64) (bool, error) { // exists (boolean)
 }
 
 func HostProcessRunning(process_name string) (bool, error) { // running (boolean)
-	// TODO: add specific logic for windows
-	// result := exec.Command("sh", "ps", "-a")
+	result := exec.Command("ps", "-a")
 	ps_output, err := result.Output()
 	if err != nil {
 		return false, err
@@ -268,7 +251,94 @@ func HostProcessRunning(process_name string) (bool, error) { // running (boolean
 	return false, nil
 }
 
+// Adapted from https://stackoverflow.com/questions/48263281/how-to-find-sshd-service-status-in-golang
+func HostServiceState(service_name string) (string, error) {
+	// returned status is one of the following:
+	// active | inactive | enabled | disabled | static | masked | alias | linked
+	// https://www.cyberciti.biz/faq/systemd-systemctl-view-status-of-a-service-on-linux/ lists all possibilities and meanings
+	cmd := exec.Command("systemctl", "check", "sshd") // ASSUMPTION: the computer uses systemd
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
+
+func NetTCPOpen(ip string, port int) (bool, error) { // exists (boolean)
+	// net.Dial or net.DialTimeout will succeed if the following succeeds:
+	/*
+	   Client -> Server: SYN
+	   Server -> Client: SYN-ACK
+	   Client -> Server: ACK
+	*/
+	conn, err := net.DialTimeout("tcp", net.JoinHostPort(ip, strconv.Itoa(port)), 10*time.Second)
+	if err != nil && !strings.HasSuffix(err.Error(), "connection refused") {
+		return false, err
+	}
+	if conn != nil {
+		defer conn.Close() // no hanging processes
+		return true, nil
+	} else {
+		return false, nil
+	}
+}
+
+func NetUDPOpen(ip string, port int) (bool, error) { // exists (boolean)
+	conn, err := net.DialTimeout("udp", net.JoinHostPort(ip, strconv.Itoa(port)), 10*time.Second)
+	// we don't really know if a udp connection is alive or not, so
+	if err != nil {
+		return false, err
+	}
+	if conn != nil {
+		defer conn.Close() // no hanging processes
+		return true, nil
+	} else {
+		return false, nil
+	}
+}
+
+func NetICMP(ip string) (bool, error) { // responded (boolean)
+	// This WILL block the thread! However, agent tasks are on their own threads.
+	result := exec.Command("ping", "-c", "5", ip) // you can write a ping packet and send it using pure golang, however it's quite complicated and requires more importing of libraries
+	ps_output, err := result.Output()
+	if err != nil {
+		return false, err
+	}
+	fmt.Println(string(ps_output))
+	ps_lines := strings.Split(string(ps_output), "\n")
+	for i := 0; i < len(ps_lines); i++ {
+		if strings.HasPrefix(ps_lines[i], "5 packets transmitted, 5 received") { // this is pretty jank
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+func FileContentString(filepath string, text string) (bool, error) { // matches
+	file_contents, read_err := ioutil.ReadFile(filepath)
+	if read_err != nil {
+		return false, read_err
+	}
+	lines := strings.Split(string(file_contents), "\n")
+	for i := 0; i < len(lines); i++ {
+		if strings.Contains(lines[i], text) {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+// https://stackoverflow.com/questions/45429210/how-do-i-check-a-files-permissions-in-linux-using-go
+func FilePermission(filepath string) (string, error) { // permissions (in the form of SRWXRWXRWX, where S is setuid bit)
+	info, err := os.Stat(filepath)
+	if err != nil {
+		return "", err
+	}
+	return info.Mode().String(), nil
+}
+
 func main() {
 	fmt.Println("windows")
-	fmt.Println(FileExists("C:\\Users\\The Power\\Documents\\2021Fall\\CMSC451\\LaForge\\laforge\\grpc\\agent\\agent_windows.go"))
+	fmt.Println(UserExists("piero"))
+	// fmt.Println(FileExists("C:\\Users\\The Power\\Documents\\2021Fall\\CMSC451\\LaForge\\laforge\\grpc\\agent\\agent_windows.go"))
 }
