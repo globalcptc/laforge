791856ed9a04e41de3caca2a6c1b918757151259
Nick Graca <njg7716@rit.edu>
2022-02-14 22:09:16 -0500

    I think the teardown host is complete now. Also added somemore to deployhost and deploynetwork.

--- builder/aws/aws.go
+++ builder/aws/aws.go
@@ -20,6 +20,9 @@ const (
 )
 
 type AWSBuilder struct {
+	AWS_Access_Key_Id     string
+	AWS_Secret_Access_Key string
+	Region                string
 }
 
 // EC2CreateInstanceAPI defines the interface for the RunInstances function.
@@ -29,18 +32,52 @@ type EC2CreateInstanceAPI interface {
 		optFns ...func(*ec2.Options)) (*ec2.RunInstancesOutput, error)
 }
 
+func (builder AWSBuilder) generateBuildID(build *ent.Build) string {
+	buildId, err := build.ID.MarshalText()
+	if err != nil {
+		buildId = []byte(fmt.Sprint(build.Revision))
+	}
+	return fmt.Sprintf("%s", buildId)
+}
+
+func (builder AWSBuilder) generateVmName(competition *ent.Competition, team *ent.Team, host *ent.Host, build *ent.Build) string {
+	return (competition.HclID + "-Team-" + fmt.Sprintf("%02d", team.TeamNumber) + "-" + host.Hostname + "-" + builder.generateBuildID(build))
+}
+
+func (builder AWSBuilder) generateRouterName(competition *ent.Competition, team *ent.Team, build *ent.Build) string {
+	return (competition.HclID + "-Team-" + fmt.Sprintf("%02d", team.TeamNumber) + "-" + builder.generateBuildID(build))
+}
+
+func (builder AWSBuilder) generateNetworkName(competition *ent.Competition, team *ent.Team, network *ent.Network, build *ent.Build) string {
+	return (competition.HclID + "-Team-" + fmt.Sprintf("%02d", team.TeamNumber) + "-" + network.Name + "-" + builder.generateBuildID(build))
+}
+
 func (builder AWSBuilder) DeployHost(ctx context.Context, provisionedHost *ent.ProvisionedHost) (err error) {
 
+	// Get information about host from ENT
 	host, err := provisionedHost.QueryProvisionedHostToHost().Only(ctx)
 	if err != nil {
 		return err
 	}
 
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-2"))
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(builder.Region))
 	if err != nil {
 		return err
 	}
+	build, err := provisionedHost.QueryProvisionedHostToPlan().QueryPlanToBuild().Only(ctx)
+	if err != nil {
+		return
+	}
+	competition, err := build.QueryBuildToCompetition().Only(ctx)
+	if err != nil {
+		return
+	}
+	team, err := provisionedHost.QueryProvisionedHostToProvisionedNetwork().QueryProvisionedNetworkToTeam().Only(ctx)
+	if err != nil {
+		return
+	}
 
+	// Describe the host with info from above and get ready to deploy
 	client := ec2.NewFromConfig(cfg)
 
 	var numInstances int32 = 1
@@ -67,9 +104,9 @@ func (builder AWSBuilder) DeployHost(ctx context.Context, provisionedHost *ent.P
 	case "ubuntu":
 		ami = ""
 	}
-
+	vmName := builder.generateVmName(competition, team, host, build)
 	//Determine the Team so I can determine the VPC to deploy to
-	var vpc []string
+	var vpc []string //TODO: Find out how to get the VPC ID
 
 	input := &ec2.RunInstancesInput{
 		ImageId:          aws.String(ami),
@@ -77,12 +114,21 @@ func (builder AWSBuilder) DeployHost(ctx context.Context, provisionedHost *ent.P
 		MinCount:         &numInstances,
 		MaxCount:         &numInstances,
 		SecurityGroupIds: vpc,
+		ClientToken:      &vmName,
+		//PrivateIpAddress: ,
+		//SecurityGroups: ,
+		//SecurityGroupIds: ,
+		//SubnetId: ,
+		//NetworkInterfaces: ,
+		//KeyName: ,
 	}
 
+	// Deploy Host
 	result, err := client.RunInstances(ctx, input)
 	if err != nil {
 		return err
 	}
+
 	id := *result.Instances[0].InstanceId
 	fmt.Println("Created tagged instance with ID " + id)
 
@@ -91,36 +137,25 @@ func (builder AWSBuilder) DeployHost(ctx context.Context, provisionedHost *ent.P
 }
 
 func (builder AWSBuilder) DeployNetwork(ctx context.Context, provisionedNetwork *ent.ProvisionedNetwork) (err error) {
-	entBuild, err := provisionedNetwork.QueryProvisionedNetworkToBuild().Only(ctx)
-	if err != nil {
-		return fmt.Errorf("couldn't query build from network \"%s\": %v", provisionedNetwork.Name, err)
-	}
-	entEnvironment, err := provisionedNetwork.QueryProvisionedNetworkToBuild().QueryBuildToEnvironment().Only(ctx)
-	if err != nil {
-		return fmt.Errorf("couldn't query build from network \"%s\": %v", provisionedNetwork.Name, err)
-	}
-	entCompetition, err := entEnvironment.EnvironmentToCompetition(ctx)
-	if err != nil {
-		return fmt.Errorf("couldn't query build from environment \"%s\": %v", entEnvironment.Name, err)
-	}
+
+	// Get information about Network from ENT
 	entNetwork, err := provisionedNetwork.QueryProvisionedNetworkToNetwork().Only(ctx)
 	if err != nil {
 		return fmt.Errorf("couldn't query build from network \"%s\": %v", provisionedNetwork.Name, err)
 	}
-	entTeam, err := provisionedNetwork.QueryProvisionedNetworkToTeam().Only(ctx)
-	if err != nil {
-		return fmt.Errorf("couldn't query build from network \"%s\": %v", provisionedNetwork.Name, err)
-	}
 
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-2"))
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(builder.Region))
 	if err != nil {
 		return err
 	}
 
+	// Describe the network with info from above and get ready to deploy
 	client := ec2.NewFromConfig(cfg)
 	input := &ec2.CreateVpcInput{
 		CidrBlock: aws.String(entNetwork.Cidr),
 	}
+
+	// Deploy Network
 	results, err := client.CreateVpc(ctx, input)
 	if err != nil {
 		return err
@@ -130,6 +165,41 @@ func (builder AWSBuilder) DeployNetwork(ctx context.Context, provisionedNetwork 
 }
 
 func (builder AWSBuilder) TeardownHost(ctx context.Context, provisionedHost *ent.ProvisionedHost) (err error) {
+	// Get information about host from ENT
+	host, err := provisionedHost.QueryProvisionedHostToHost().Only(ctx)
+	if err != nil {
+		return err
+	}
+	build, err := provisionedHost.QueryProvisionedHostToPlan().QueryPlanToBuild().Only(ctx)
+	if err != nil {
+		return
+	}
+	competition, err := build.QueryBuildToCompetition().Only(ctx)
+	if err != nil {
+		return
+	}
+	team, err := provisionedHost.QueryProvisionedHostToProvisionedNetwork().QueryProvisionedNetworkToTeam().Only(ctx)
+	if err != nil {
+		return
+	}
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(builder.Region))
+	if err != nil {
+		return err
+	}
+	client := ec2.NewFromConfig(cfg)
+
+	vmName := builder.generateVmName(competition, team, host, build)
+	var instances []string
+	instances[0] = vmName
+	input := &ec2.TerminateInstancesInput{
+		InstanceIds: instances,
+	}
+	results, err := client.TerminateInstances(ctx, input)
+	if err != nil {
+		return err
+	}
+	fmt.Println(results)
 	return
 }
 
