d0a9bdbc832b6a6525cdd7f223f5e5c567394fd9
Jacob Doll <99jdoll@gmail.com>
2022-09-21 17:10:09 -0400

    Added ScheduleStep and ProvisionedScheduleStep to /ent/schema
    - Added /ent/schema/schedulestep.go
    - Added /ent/schmea/provisionedschedulestep.go
    - Modified /ent/schema/agenttask.go
    - Modified /ent/schema/status.go

---
+++ ent/schema/schedulestep.go
@@ -0,0 +1,62 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/dialect/entsql"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+)
+
+// ScheduleStep holds the schema definition for the ScheduleStep entity.
+type ScheduleStep struct {
+	ent.Schema
+}
+
+// Fields of the ScheduleStep.
+func (ScheduleStep) Fields() []ent.Field {
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).
+			Default(uuid.New),
+		field.Enum("type").
+			Values(
+				"Script",
+				"Command",
+				"FileDelete",
+				"FileDownload",
+				"FileExtract",
+				"Ansible",
+			),
+		field.Bool("repeated"),
+		field.Time("start_time"),
+		field.Time("end_time"),
+		field.Int("interval"),
+	}
+}
+
+// Edges of the ScheduleStep.
+func (ScheduleStep) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("ScheduleStepToStatus", Status.Type).
+			Unique().
+			Annotations(entsql.Annotation{
+				OnDelete: entsql.Cascade,
+			}),
+		edge.To("ScheduleStepToProvisionedHost", ProvisionedHost.Type).
+			Unique(),
+		edge.To("ScheduleStepToScript", Script.Type).
+			Unique(),
+		edge.To("ScheduleStepToCommand", Command.Type).
+			Unique(),
+		edge.To("ScheduleStepToFileDelete", FileDelete.Type).
+			Unique(),
+		edge.To("ScheduleStepToFileDownload", FileDownload.Type).
+			Unique(),
+		edge.To("ScheduleStepToFileExtract", FileExtract.Type).
+			Unique(),
+		edge.To("ScheduleStepToAnsible", Ansible.Type).
+			Unique(),
+		edge.From("ScheduleStepToProvisionedScheduleStep", ProvisionedScheduleStep.Type).
+			Ref("ProvisionedScheduleStepToScheduleStep"),
+	}
+}
