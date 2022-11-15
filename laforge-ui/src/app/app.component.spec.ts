87f2f08908c8d0bbe317a82f91a440af747a1041
BradHacker <harkerbd@gmail.com>
2022-11-15 11:09:35 -0500

    Set up layout unit tests

--- laforge-ui/src/app/app.component.spec.ts
+++ laforge-ui/src/app/app.component.spec.ts
@@ -6,7 +6,7 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      declarations: [AppComponent]
     }).compileComponents();
   });
 
@@ -14,20 +14,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'laforge-ui'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('laforge-ui');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'laforge-ui app is running!'
-    );
   });
 });
--- laforge-ui/src/app/components/layout/layout.component.spec.ts
+++ laforge-ui/src/app/components/layout/layout.component.spec.ts
@@ -1,23 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { LayoutComponent } from './layout.component';
 
 describe('LayoutComponent', () => {
   let component: LayoutComponent;
   let fixture: ComponentFixture<LayoutComponent>;
+  let getRandomIntSpy: jasmine.Spy<() => number>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LayoutComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule],
+      declarations: [LayoutComponent]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LayoutComponent);
+    getRandomIntSpy = spyOn(fixture.componentInstance, 'getRandomInt').and.returnValue(0);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(getRandomIntSpy).toHaveBeenCalled();
   });
 });
