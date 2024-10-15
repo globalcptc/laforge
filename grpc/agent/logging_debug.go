12c5da9d2c2eff091dc17267324d8392aa7bec59
BradHacker <harkerbd@gmail.com>
2024-10-15 14:38:53 +0000

    Add detailed logging option to laforge-agent

---
+++ grpc/agent/logging_debug.go
@@ -0,0 +1,33 @@
+// +build debug
+
+package main
+
+import (
+	"github.com/kardianos/service"
+)
+
+func logf(l service.Logger, t string, msg string, args ...interface{}) {
+	switch t {
+	case LogError:
+		l.Errorf(msg, args)
+	case LogWarning:
+		l.Warningf(msg, args)
+	case LogInfo:
+		l.Infof(msg, args)
+	default:
+		l.Errorf("Unknown log type (%s) provided: '%s' %v", t, msg, args)
+	}
+}
+
+func log(l service.Logger, t string, msg string) {
+	switch t {
+	case LogError:
+		l.Error(msg)
+	case LogWarning:
+		l.Warning(msg)
+	case LogInfo:
+		l.Info(msg)
+	default:
+		l.Errorf("Unknown log type (%s) provided: '%s'", t, msg)
+	}
+}
